b856ee03d629f68bfe418c90a46e270850a7b7be
Kálóczi Kevin <kaloczi.kvn@gmail.com>
2023-08-16 21:45:33 +0200

    init v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,34 @@
-class App {
-    constructor() {
-        this.scrollElement = document.documentElement;
-        this.nav = document.querySelector('nav');
-    
-        window.addEventListener('scroll', this.scroll.bind(this), false);
-        this.scroll();
-    }
-
-    scroll() {
-        if (this.scrollElement.scrollTop >= 1) {
-            this.nav.classList.add('scrolled');
-        } else {
-            this.nav.classList.remove('scrolled');
-        }
-    }
-}
-
 window.onload = () => {
-    new App();
+  const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
+  const cards = document.querySelectorAll(".card .card-image");
+  if (cards.length > 0) {
+    cards.forEach((card) => {
+      const fullImage = card.querySelector(".full-img");
+
+
+      card.addEventListener("mousemove", (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const height = card.offsetHeight;
+        const percentage = 100 / height * y;
+        const oneToHundred = percentage / 100;
+        
+        const fullImage_height = fullImage.offsetHeight - height;
+        const px = oneToHundred * fullImage_height;
+        fullImage.style.transform = "translateY(-" + px + "px)";
+      });
+      card.addEventListener("mouseleave", (e) => {
+        fullImage.style.transform = "translateY(0)";
+      });
+    });
+  }
 };
+
+
